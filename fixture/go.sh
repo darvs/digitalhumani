13ad5a57613329c8c619c52b9a3eccf3994f29c0
Sébastien Cabot <sebascabot@gmail.com>
2018-12-14 14:51:37 -0500

    BatchWriteItem limited to 25 Request

--- fixture/go.sh
+++ fixture/go.sh
@@ -1,5 +1,8 @@
 TARGET="_request.json"
 
+#FIXME: Max 25 'Request' in a batch,
+#       must call multiple batch of 25 'Put Request' each
+
 ./items2request.js > ${TARGET}
 aws dynamodb batch-write-item --request-items file://${TARGET}
 rm ${TARGET}
